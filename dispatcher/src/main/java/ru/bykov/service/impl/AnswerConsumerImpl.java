b234947972b2cd98c1f23ff61b4774fc41d59ee6
Ivan <bukov123@yandex.ru>
2022-12-21 15:43:50 +0300

    update consumer and producer of s

---
+++ dispatcher/src/main/java/ru/bykov/service/impl/AnswerConsumerImpl.java
@@ -0,0 +1,25 @@
+package ru.bykov.service.impl;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.bykov.controller.UpdateController;
+import ru.bykov.service.AnswerConsumer;
+
+import static ru.bykov.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class AnswerConsumerImpl implements AnswerConsumer {
+
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
+    public void consumer(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+    }
+}
---
+++ node/src/main/java/ru/bykov/service/ConsumerService.java
@@ -0,0 +1,9 @@
+package ru.bykov.service;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public interface ConsumerService {
+    void consumerTextMessageUpdate(Update update);
+    void consumerDocMessageUpdate(Update update);
+    void consumerPhotoMessageUpdate(Update update);
+}
